9f0c6af8e4be3cc7fe77678bbf9d5b05250fa0e3
Révész Ádám <reveszadam@gmail.com>
2018-08-09 23:29:12 +0200

    Initial request parsing with example server

---
+++ request.py
@@ -0,0 +1,50 @@
+import re
+
+def _parseQueryParams(queryString):
+    params = queryString.split('&')
+    valids = [a.split('=') for a in params if len(a.split('=')) == 2]
+    queryParams = {}
+    for pair in valids :
+        queryParams[pair[0]] = pair[1]
+    return queryParams
+
+def _getPathAndQueryParams(fullPath):
+    ind = fullPath.find('?')
+    path = fullPath[:ind] if ind > -1 else fullPath
+    queryParams = _parseQueryParams(fullPath[(ind + 1):]) if ind > -1 else {}
+    return (path, queryParams)
+
+def requestFromString(req):
+    lines = req.split('\r\n')
+    method, path, queryParams, httpVer = '', '', {}, ''
+    args = lines[0].split(' ')
+    method = args[0].upper()
+    httpVer = args[2]
+    (path, queryParams) = _getPathAndQueryParams(args[1])
+    bodyStartInd = lines.index('')
+
+    headers = {}
+    for line in lines[1:bodyStartInd]:
+        m = re.search('^([^:]+):(.*)$', line)
+        if m is None :
+            continue
+        key = m.group(1).strip()
+        value = m.group(2).strip()
+        headers[key] = value
+
+    body = "\r\n".join(lines[(bodyStartInd + 1):]) if bodyStartInd > -1 else ""
+
+    return Request(method, path, {}, queryParams, httpVer, headers, body)
+
+class Request:
+    """HTTP request"""
+
+    def __init__(self, method="GET", path="/", params={}, queryParams={},
+                 httpVer="", headers={}, body=""):
+        self.method = method
+        self.path = path
+        self.params = params,
+        self.queryParams = queryParams
+        self.httpVer = httpVer
+        self.headers = headers
+        self.body = body
---
+++ server.py
@@ -0,0 +1,34 @@
+import sys
+import socket
+import request
+
+port = int(sys.argv[1] if len(sys.argv) > 1 else 8080)
+print("Running on port {}".format(port))
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind(('', port))
+listen_socket.listen(1)
+
+while True:
+    connection, client_address, = listen_socket.accept()
+    req = connection.recv(1024)
+    httpRequest = request.requestFromString(req)
+    print(httpRequest.path)
+    print(httpRequest.queryParams)
+    print(httpRequest.headers)
+
+    # print(req.split('\r\n'))
+    body = '{"a":123}'
+
+    http_response = """\
+HTTP/1.1 200 OK
+Content-Type: application/json; charset=utf-8
+Content-Length: {}
+
+{}""".format(len(body), body)
+    print("------------")
+    print(http_response)
+
+    connection.sendall(http_response)
+    connection.close()
