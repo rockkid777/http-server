e09de277c1a9ae5b902c7a5848efbf8a1ba56257
Révész Ádám <reveszadam@gmail.com>
2018-08-25 09:35:55 +0200

    added Route

---
+++ http-server/route.py
@@ -0,0 +1,19 @@
+import re
+
+class Route:
+
+    def __init__(self, method, pathPrefix, pathExp, handler):
+        self.method = method.upper()
+        self.regex = Route.makePathRegExp(pathPrefix, pathExp)
+        self.handler = handler
+
+    @staticmethod
+    def makePathRegExp(pathPrefix, pathExp):
+        def conv(chunk):
+            if len(chunk) < 2 or chunk[0] != ':' :
+                return chunk
+            name = chunk[1:]
+            return "(?P<{}>[^/]+)".format(name)
+        suffix = "/" if pathExp[-1] == "/" else ""
+        exp = "/".join(map(conv, pathExp.split('/')[1:])) + suffix
+        return pathPrefix + exp
