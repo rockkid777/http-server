d4d5a79983faac7344fbc58c77559ad7f8a3917f
Révész Ádám <reveszadam@gmail.com>
2018-08-31 18:32:57 +0200

    fixed typos and wrong calls in router

--- http_server/router.py
+++ http_server/router.py
@@ -19,31 +19,49 @@ class Router:
         # for override
         pass
 
+    def addRoute(self, method, path, handler):
+        if method == "GET" :
+            routes = self.get
+        elif method == "POST" :
+            routes = self.post
+        elif method == "PUT" :
+            routes = self.put
+        elif method == "PATCH" :
+            routes = self.patch
+        elif method == "DELETE" :
+            routes = self.delete
+        else :
+            return
+        route = Route(method, self.pathPrefix, path, handler)
+        routes.append(route)
+
     def loadRoutes(self):
-        for router in routers :
+        for router in self.routers :
             router.loadRoutes()
-        for controller in controllers :
+        for controller in self.controllers :
             controller.registerHandlers(self)
 
     def _local_route(self, routes, request, response):
         for route in routes :
-            if route.match(request.path) :
+            m = route.regex.match(request.path)
+            if m :
+                request.params = m.groupdict()
                 route.handler(request, response)
                 return True
         return False
 
-    def route(self, request, response = Response().NotFound):
-        for router in routers :
+    def route(self, request, response = Response(status=Status.NotFound())):
+        for router in self.routers :
             if router.route(request, response) :
                 return True
         if request.method == "GET" :
-            return _local_route(self.get, request, response)
+            return self._local_route(self.get, request, response)
         if request.method == "POST" :
-            return _local_route(self.post, request, response)
+            return self._local_route(self.post, request, response)
         if request.method == "PUT" :
-            return _local_route(self.put, request, response)
+            return self._local_route(self.put, request, response)
         if request.method == "PATCH" :
-            return _local_route(self.patch, request, response)
+            return self._local_route(self.patch, request, response)
         if request.method == "DELETE" :
-            return _local_route(self.delete, request, response)
+            return self._local_route(self.delete, request, response)
         return False
